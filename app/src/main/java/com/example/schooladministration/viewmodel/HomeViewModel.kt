70854eadd7379f33cee34ee7253dbce5e220b125
haiderhassan66 <haiderhassan66@gmail.com>
2023-08-28 21:51:34 +0500

    home screen

---
+++ app/src/main/java/com/example/schooladministration/viewmodel/HomeViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.schooladministration.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel:ViewModel() {
+    fun clickPaper(){
+
+    }
+    fun clickStudent(){
+
+    }
+    fun clickCounsultant(){
+
+    }
+    fun clickQuiz(){
+
+    }
+    fun clickProfile(){
+
+    }
+    fun clickLogout(){
+
+    }
+}
